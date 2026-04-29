3470d8144eb42d9bb77bafb2739806a7366f24d6
Aditya Janawade <adityajanawade950@gmail.com>
2026-04-29 15:19:51 +0530

    fixed PORT error

--- backend/server.js
+++ backend/server.js
@@ -42,7 +42,7 @@ app.get("/all", async (req, res) => {
 });
 
 // Start server
-
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log("Server running on port " + PORT);
